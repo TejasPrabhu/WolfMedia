6d542208e413911b7b8494499dc1a38ba5f72daf
TejasPrabhu <tejas.prabhu29@gmail.com>
2023-08-30 18:55:15 -0400

    Add config files

---
+++ src/main/java/com/tejasprabhu/wolfmedia/config/CorsConfig.java
@@ -0,0 +1,41 @@
+package com.tejasprabhu.wolfmedia.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    @Profile("dev")
+    public CorsFilter corsFilterDev() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("*");
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    @Profile("prod")
+    public CorsFilter corsFilterProd() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("https://myproductiondomain.com");
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("Authorization");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsFilter(source);
+    }
+}
